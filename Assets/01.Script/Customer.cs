db78b66a8caec72bbb5561b3a3bb31baccc58f95
LKM0222 <python03@naver.com>
2023-05-23 11:05:11 +0900

    손님 프리펩화 및 움직임 1차작업

--- Assets/01.Script/Customer.cs
+++ Assets/01.Script/Customer.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class Customer{
+[System.Serializable] public class Customer{
     string name;
     bool gender;
     int favoriteGame; //code
