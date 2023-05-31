3d6efacf9e8abf780278644d197e691ff3ace7b8
LKM0222 <python03@naver.com>
2023-05-31 16:12:33 +0900

    손님이 이동하는 거리를 큐 구조로 처리하기위한 기초작업

--- Assets/01.Script/CustomerMoving.cs
+++ Assets/01.Script/CustomerMoving.cs
@@ -2,14 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class MyQueue{
+    private Queue<Transform> _queue = new Queue<Transform>();
+
+    public void Enqueue(Transform _pos){
+        _queue.Enqueue(_pos);
+    }
+
+    public Transform Dequeue(){
+        return _queue.Dequeue();
+    }
+
+    public Transform Pick(){
+        return _queue.Peek();
+    }
+
+    public int Count(){
+        return _queue.Count;
+    }
+}
+
 public class CustomerMoving : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 0.5f;
     [SerializeField] bool moveFlag = true;
 
+    //움직임 리스트를 큐 구조로 구현하는게 좋을듯?
+
+
     public Vector3 target;
     
-    Customer customer = new Customer("test",true, 10001, 20001, 30001, 160, 4.5f);
+    
+    //Customer customer = new Customer("test",true, 10001, 20001, 30001, 160, 4.5f);
     // Start is called before the first frame update
     void Start()
     {
